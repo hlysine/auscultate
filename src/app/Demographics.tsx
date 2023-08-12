4d2a609c5dfbdeb846a08c27948e7064f92f5e34
Henry Lin <henry.ys.lin@gmail.com>
2023-08-12 11:10:58 +0800

    Improve demographics wrapping on small screen

--- src/app/Demographics.tsx
+++ src/app/Demographics.tsx
@@ -26,20 +26,24 @@ export default function Demographics({
   patient,
 }: DemographicsProps): JSX.Element {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-wrap gap-4 justify-between">
+    <div className="flex gap-4 flex-wrap justify-between">
+      <div className="flex flex-col gap-4">
         <DataItem label="ID" value={patient.patientId.toString()} />
+        <DataItem label="Dataset" value={patient.campaign} />
+      </div>
+      <div className="flex flex-col gap-4">
         <DataItem label="Age" value={patient.age} />
+        <DataItem label="Sex" value={patient.sex} />
+      </div>
+      <div className="flex flex-col gap-4">
+        <DataItem label="Weight" value={patient.weight?.toString()} unit="kg" />
         <DataItem label="Height" value={patient.height?.toString()} unit="cm" />
+      </div>
+      <div className="flex flex-col gap-4">
         <DataItem
           label="Is pregnent"
           value={patient.isPregnant ? 'Yes' : 'No'}
         />
-      </div>
-      <div className="flex flex-wrap gap-4 justify-between">
-        <DataItem label="Dataset" value={patient.campaign} />
-        <DataItem label="Sex" value={patient.sex} />
-        <DataItem label="Weight" value={patient.weight?.toString()} unit="kg" />
         <DataItem
           label="Additional ID"
           value={patient.additionalId?.toString()}
